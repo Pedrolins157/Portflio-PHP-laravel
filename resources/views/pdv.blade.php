648a3704c787b56f1f1ea4ad43debad5b663a5a6
pedrolins157 <pedrolins2@gmail.com>
2023-10-24 13:49:51 -0300

    finalizando o front d pdv

--- resources/views/pdv.blade.php
+++ resources/views/pdv.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <title>Gerenciamento Lins</title>
     <link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -18,26 +17,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css">
 </head>
 
-<body style="background-color: #e7e7ec;">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light text-light">
-        <a class="navbar-brand" href="#"><img src="img/icon-lins.png" alt="" style="border-radius:50%;"> Nome da Empresa</a>
+<body style="background-color: #f6f6ff;">
+    <nav class="navbar navbar-expand-lg navbar-light  text-light" style="background-color: #060533db  !important;">
+        <a class="navbar-brand text-light" href="#"><img src="img/icon-lins.png" alt="" style="border-radius:50%;"> Nome da Empresa</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse mr-4" id="navbarNav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active ml-2">
-                    <a class="nav-link" href="#" style="border-radius:20%;background-color:#00bd0082;"><img src="img/scanner.png" alt=""></a>
-                </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><img src="img/impressora.png" width="26px" height="26px" alt=""></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><img src="img/user-venda.png" alt=""></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="#"><img src="img/logout.png" alt=""></a>
+                    <a class="nav-link text-light efeito"style="border-radius:50%;" href="#"><img src="img/logout.png" alt=""> Sair</a>
                 </li>
             </ul>
         </div>
@@ -52,47 +41,40 @@
                     <input type="text" class="form-control " style="height:60px;font-size: 24px;" placeholder="Nome do produto ou código de barras">
                 </div>
                 <div class="row">
-
                     <div class="col-6 mt-3">
                         <h3 class="text-gray-800 ">Quantidade </h3>
                     </div>
-
                     <div class="col-6 mt-3">
                         <h3 class="text-gray-800 ">Preço Unitário </h3>
                     </div>
                 </div>
                 <div class="row">
-
                     <div class="col-5 mr-3">
                         <input type="text" class="form-control " style="height:60px;font-size: 24px;" id="valor">
                     </div>
-
                     <div class="col-5 ml-5">
                         <input type="text" class="form-control " style="height:60px;font-size:24px;width:110%;" id="valor" oninput="formatarMoeda(this)" placeholder="00,00">
                     </div>
-
                 </div>
-
-
                 <div class="row mt-3">
                     <h3 class="text-gray-800 ml-3">Total </h3>
                 </div>
                 <div class="row col-12 ">
-                    <input type="text" class="form-control " style="height:60px;font-size: 24px;" oninput="formatarMoeda(this)" placeholder="00,00">
+                    <label class="form-control" style="height: 60px; font-size: 24px; background: #000044c9; color: #ffff;padding-top: 10px;">
+                        <span id="customText" class="text-light ">00,00</span>
+                    </label>
+
                 </div>
-
-
             </div>
-
             <div class="col">
                 <h3 class="text-gray-800">Resumo da venda</h3>
                 <table class="table table-bordered text-gray-900">
                     <thead>
                         <tr>
-                            <th scope="col">Qtd</th>
-                            <th scope="col">Produto</th>
-                            <th scope="col">Valor Und.</th>
-                            <th scope="col">Total</th>
+                            <th scope="col" style="background: #7052ff;color: #f3f0ff;"> Qtd</th>
+                            <th scope="col" style="background: #7052ff;color: #f3f0ff;">Produto</th>
+                            <th scope="col" style="background: #7052ff;color: #f3f0ff;">Valor Und.</th>
+                            <th scope="col" style="background: #7052ff;color: #f3f0ff;">Total</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -116,26 +98,54 @@
                         </tr>
                     </tbody>
                 </table>
-
-                <div class="row ml-1 justify-content-evelyn">
+                <div class="row ml-1 justify-content-evelyn " style="margin-top: 37px;">
                     <div class="col-1 p-0">
                         <h4 class="text-gray-900 mt-3">Total</h4>
                     </div>
                     <div class="row col-4 mr-5">
-                        <input type="text" class="form-control" style="height: 60px; font-size: 24px;" oninput="formatarMoeda(this)" placeholder="00,00">
+                        <label class="form-control" style="height: 60px; font-size: 24px; background: #00044491;; color: #ffff;padding-top: 10px;">
+                            <span id="customText" class="text-light ">00,00</span>
+                        </label>
                     </div>
                     <div class="row col-3 ml-5">
-                        <button class="btn btn-control btn-danger">Cancelar venda</button>
+                        <button class="btn btn-control btn-danger efeito">[ F9 ] Cancelar</button>
                     </div>
                     <div class="row col-3 ml-1">
-                         <button class="btn btn-control btn-success">[ F12 ] Pagamento</button>
+                        <button class="btn btn-control btn-success efeito" style="background:#1d8a36ad;">[ F12 ] Pagamento</button>
                     </div>
                 </div>
-
             </div>
         </div>
+        <div class="container mb-5" style="max-width:90%;margin-top:100px;">
+            <ul class="nav justify-content-between">
+                <li class="nav-item ">
+                    <a class="nav-link  btn btn-cadastrar-usuario text-light efeito" style="height: 60px; font-size: 24px;" href="#" ><img class="mb-1" src="img/icon-tittle(1).png" width="32px" height="32px" alt="">Histórico de vendas [ F1 ]</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link btn text-light efeito btn-cadastrar-usuario" style="height: 60px; font-size: 24px;" href="#"><img class="mb-1" src="img/icon-tittle(1).png" width="32px" height="32px" alt="">Caixa [ F2 ]</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link btn text-light efeito btn-cadastrar-usuario" style="height: 60px; font-size: 24px;" href="#"><img class="mb-1" src="img/cliente-consulta.png" width="32px" height="32px" alt=""> Cliente [ F3 ]</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn text-light efeito btn-cadastrar-usuario" style="height: 60px; font-size: 24px;" href="#"><img src="img/user-venda.png" alt=""> Vendedor [ F4 ]</a>
+                </li>
+            </ul>
+        </div>
 
-        @include('layout.parte.footer')
+        <footer class="sticky-footer checked-day modo-diurno  text-light fixed-bottom" style="background:#02010d;">
+            <script src="{{url('js/jquery.min.js')}}"></script>
+            <script src="{{url('js/bootstrap.bundle.min.js')}}"></script>
+            <script src="{{url('js/jquery.easing.min.js')}}"></script>
+            <script src="{{url('js/sb-admin-2.min.js')}}"></script>
+            <script src="{{url('js/script.js')}}"></script>
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span><b>Copyright &copy; </b>Pedro & Josiane Lins - Se amam!</span>
+                </div>
+            </div>
+        </footer>
+
 </body>
 
 </html>
